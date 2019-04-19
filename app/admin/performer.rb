8f195682be8d0d5d8004127f89977c0fa85e4ad3
tomwey <tomwey@163.com>
2019-04-19 09:03:56 +0800

    ccc

--- app/admin/performer.rb
+++ app/admin/performer.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Performer do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 permit_params :name, :avatar, :mobile, :_type, :school, :bio, :height, :weight, :phone, :nickname,
-:sex,:age,:nation,:edu_level,:speciality,:marry_type,:now_job,:interest,:source,:body_size,:chest_size,:address, :sort, :promo_score, :user_id :waist_size,:hip_size,:vision,:hair_style,:hair_color,:footcode,:skills,:trainings, { photos: [] }, { tags: [] }
+:sex,:age,:nation,:edu_level,:speciality,:marry_type,:now_job,:interest,:source,:body_size,:chest_size,:address, :sort, :promo_score, :user_id, :waist_size,:hip_size,:vision,:hair_style,:hair_color,:footcode,:skills,:trainings, { photos: [] }, { tags: [] }
 #
 # or
 #
